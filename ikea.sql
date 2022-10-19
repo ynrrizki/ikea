ec0b6c0c0b3179bf9eddf92cca0606b9724b064a
ynrrizki <yanuarrizki165@gmail.com>
2022-10-19 10:01:13 +0700

    add file sql

--- ikea.sql
+++ ikea.sql
@@ -1,33 +1,149 @@
-CREATE TABLE `User` (
-  `id` int PRIMARY KEY,
-  `username` varchar(255),
-  `password` varchar(255),
-  `level` enum
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0-dev+20220304.7fed200089
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 19, 2022 at 03:00 AM
+-- Server version: 8.0.27
+-- PHP Version: 8.1.7
 
-CREATE TABLE `Barang` (
-  `id` int PRIMARY KEY,
-  `nama` varchar(255),
-  `stok` int,
-  `harga` int,
-  `created_at` timestamps,
-  `updated_at` timestamps
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `Pesanan` (
-  `id` int PRIMARY KEY,
-  `transaksi_id` int,
-  `jumlah` int,
-  `total` int
-);
 
-CREATE TABLE `Transaksi` (
-  `id` int PRIMARY KEY,
-  `nama` varchar(255),
-  `total` int,
-  `bayar` int,
-  `created_at` timestamps,
-  `updated_at` timestamps
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-ALTER TABLE `Transaksi` ADD FOREIGN KEY (`id`) REFERENCES `Pesanan` (`transaksi_id`);
+--
+-- Database: `ikea`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` bigint NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `stok` int NOT NULL,
+  `harga` int NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `updated_at` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id`, `nama`, `stok`, `harga`, `created_at`, `updated_at`) VALUES
+(1, 'TROTTEN', 500, 2999000, '2022-10-19 02:15:04', '2022-10-19 02:15:04'),
+(2, 'HAUGA', 500, 2999000, '2022-10-19 02:15:04', '2022-10-19 02:15:04'),
+(3, 'GURSKEN', 500, 999000, '2022-10-19 02:16:17', '2022-10-19 02:16:17'),
+(4, 'MALM', 500, 9999000, '2022-10-19 02:16:17', '2022-10-19 02:16:17'),
+(5, 'GLADSTAD', 500, 2999000, '2022-10-19 02:17:23', '2022-10-19 02:17:23'),
+(6, 'ENHET', 500, 1400000, '2022-10-19 02:17:23', '2022-10-19 02:17:23'),
+(7, 'LANBERG', 500, 4199000, '2022-10-19 02:18:09', '2022-10-19 02:18:09'),
+(8, 'SVENBERTIL', 500, 695000, '2022-10-19 02:18:09', '2022-10-19 02:18:09');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `id` bigint NOT NULL,
+  `transaksi_id` bigint NOT NULL,
+  `jumlah` int NOT NULL,
+  `total` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` bigint NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `total` int NOT NULL,
+  `bayar` int NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `updated_at` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `level` enum('ADMIN','KASIR') COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `level`) VALUES
+(1, 'admin', 'admin', 'ADMIN'),
+(2, 'kasir', 'kasir', 'KASIR');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
